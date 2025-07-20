8712ca99c3c254b3263490abc24bfdaaf8effcd4
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2025-07-20 10:07:01 +0530

    Login page

---
+++ src/Login.jsx
@@ -0,0 +1,32 @@
+// src/Login.jsx
+import React from "react";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+
+const clientId =
+  "260551266904-s5p0g8452l1ldhnu084g5ckvn4s4ai1h.apps.googleusercontent.com"; // replace with actual ID
+
+function Login() {
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "100px",
+        }}
+      >
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log("Login Success:", credentialResponse);
+            // Save token in state, context, or localStorage
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </div>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default Login;
